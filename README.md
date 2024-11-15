6589d5fb83dd37f1df950fbbd34dfe17ad983b73
Ilya Penrenckov <petilia2002@yandex.ru>
2024-11-15 18:57:46 +0300

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularCrashCourse
+# Angular Crash Course
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.12.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { MyFirstComponent } from './components/my-first/my-first.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MyFirstComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-crash-course';
